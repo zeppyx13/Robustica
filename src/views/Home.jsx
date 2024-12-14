ea4815aed0ea04669677be5b1fb88947381b1dfd
Gung Nanda <gungnands@gmail.com>
2024-12-15 02:09:55 +0800

    why us section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -14,11 +14,11 @@ const Home = () => {
         {/* Hero Section */}
         <section
           id="hero"
-          className="relative bg-center bg-cover bg-no-repeat h-96 flex items-center justify-center"
+          className="relative bg-center bg-cover bg-no-repeat h-96 flex items-center justify-center shadow-lg"
           style={{ backgroundImage: "url('./Assets/beans2.jpg')" }}
         >
           {/* Overlay */}
-          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+          <div className="absolute inset-0 bg-black bg-opacity-50 shadow-lg"></div>
 
           {/* Content */}
           <div className="relative z-10 container mx-auto px-4 flex flex-col items-center justify-center h-full text-center text-white">
@@ -36,7 +36,6 @@ const Home = () => {
             </a>
           </div>
         </section>
-
         {/* Signature Section */}
         <section className="py-16" id="Signature">
           <div className="text-center mb-12">
@@ -121,6 +120,156 @@ const Home = () => {
             </Swiper>
           </div>
         </section>
+        {/* why us */}
+        <section className="py-16" id="why-us">
+          <div className="container mx-auto">
+            {/* Header Section */}
+            <div className="text-center mb-2">
+              <h2 className="text-3xl md:text-5xl font-bold text-gray-900">
+                Why Choose Us?
+              </h2>
+            </div>
+
+            {/* Content Section */}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center mt-10">
+              {/* Left Column */}
+              <div className="space-y-4 px-5">
+                {/* Card 1 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/farmer.png"
+                      alt="farmer"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      Support Local Farmers
+                    </h3>
+                    <p className="text-gray-600 text-sm">
+                      We work directly with local farmers to ensure that they
+                      receive a fair price for their beans.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Card 2 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/coffee-beans.png"
+                      alt="coffee-beans"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      Best beans quality
+                    </h3>
+                    <p className="text-gray-600 text-sm">
+                      We source the best quality beans from around the world to
+                      ensure that you get the bag's of coffee.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Card 3 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/coffee-cup.png"
+                      alt="coffee-cup"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      Unique Flavor Profile
+                    </h3>
+                    <p className="text-gray-600 text-sm">
+                      Our beans are grown at high altitudes, which gives them a
+                      unique flavor profile that you won't find anywhere else.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Center Image */}
+              <div className="flex justify-center px-4">
+                <img
+                  src="./Logo/logo-no-background.png"
+                  alt="Robustica Logo"
+                  className="w-[75%] max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl object-contain"
+                />
+              </div>
+
+              {/* Right Column */}
+              <div className="space-y-8">
+                {/* Card 4 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/price-tag.png"
+                      alt="Arabica Flores Bajawa"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      Best price guarantee
+                    </h3>
+                    <p className="text-gray-600 text-sm">
+                      We offer the best prices on the market, so you can enjoy
+                      your favorite coffee without breaking the bank.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Card 5 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/offee-plant.png"
+                      alt="coffee-plant"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      From the best plantation
+                    </h3>
+                    <p className="text-gray-600 text-sm">
+                      Our beans are sourced from the best plantations in the
+                      world, so you can be sure that you're getting the best
+                      quality coffee.
+                    </p>
+                  </div>
+                </div>
+
+                {/* Card 6 */}
+                <div className="flex items-start space-x-4">
+                  <div>
+                    <img
+                      src="./Icon/coffee-bag.png"
+                      alt="coffee-plant"
+                      className="w-full h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-bold text-gray-900">
+                      Many variants of coffee beans
+                    </h3>
+                    <p className="text-gray-600">
+                      We offer a wide range of coffee beans, so you can find the
+                      perfect one for your taste.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         {/* Product Section */}
         <section className="relative bg-center bg-cover bg-no-repeat pt-5 pb-6">
           <div className="relative z-10 container mx-auto px-4">
@@ -181,13 +330,11 @@ const Home = () => {
             </div>
           </div>
         </section>
-
         {/* Journey Section */}
         <section
           className="relative bg-center bg-cover bg-no-repeat py-16 lg:py-20"
           style={{
             backgroundImage: "url('./Assets/plants.png')",
-            backgroundColor: "#ede1d1", // Warna krem untuk melengkapi tema latar
           }}
         >
           {/* Overlay */}
