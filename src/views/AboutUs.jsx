c4821b132f9dd1f11df3e34691611f2cea768dc5
Gung Nanda <gungnands@gmail.com>
2024-12-16 12:46:42 +0800

    fixing wrapper

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -6,7 +6,7 @@ import About from "../components/Section/About";
 
 const AboutUs = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden">
       <Navbar />
       <Hero />
       <About />
